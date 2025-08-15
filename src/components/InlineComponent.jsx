bad27b94e00517050f2e8e52f66ddf38d588f8eb
keshavnagar <keshavnagar887@gmail.com>
2025-08-15 17:06:30 +0530

    add style sheet wali css

--- src/components/InlineComponent.jsx
+++ src/components/InlineComponent.jsx
@@ -1,8 +1,9 @@
+import '..css/style.css';
 const header = { color: "pink", fontSize: "6rem" };
 const InlineComponent = () => {
   return (
     <>
-      <h1 style={header}>Inlinne Component</h1>
+      <h1 className="header">Inlinne Component</h1>
     </>
   );
 };
