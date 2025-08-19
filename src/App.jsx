3e6663e7ae822dd38c1661133450a443a1b782f0
keshavnagar <keshavnagar887@gmail.com>
2025-08-19 23:22:26 +0530

    remove some line of inline and outline components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
 import Todo from "./components/Todo";
-import InlineComponent from "./components/InlineComponent";
-import OutlineComponent from "./components/OutlineComponent";
 const App = () => {
   return (
     <>
-      {/* <Todo></Todo> */}
-      <InlineComponent></InlineComponent>
-      <OutlineComponent></OutlineComponent>
+      <Todo></Todo>
     </>
   );
 };
