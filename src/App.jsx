0f43ea2c4fd3b73eac9d0128928f5d7f82a62277
keshavnagar <keshavnagar887@gmail.com>
2025-08-19 23:27:19 +0530

    add header component in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import Todo from "./components/Todo";
+import Header from "./components/Header";
 const App = () => {
   return (
     <>
+      <Header></Header>
       <Todo></Todo>
     </>
   );
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <>
+      <h1>My To Do List</h1>
+    </>
+  );
+};
+
+export default Header;
