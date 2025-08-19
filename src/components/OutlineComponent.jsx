b62a06091d6e1962fe8e6ea74d16ec95bcc19f2e
keshavnagar <keshavnagar887@gmail.com>
2025-08-19 22:53:39 +0530

    deleted the practical files of inline and outline component with their css files

--- src/components/OutlineComponent.jsx
+++
@@ -1,9 +0,0 @@
-import styles from './OutlineComponent.module.css'
-const OutlineComponent = () => {
-  return (
-    <>
-      <h1 className={styles.header}>Outline Component</h1>
-    </>
-  );
-};
-export default OutlineComponent;
